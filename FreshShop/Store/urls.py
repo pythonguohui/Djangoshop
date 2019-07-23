ffb96d46095cf5cf4fcb3958275c3d5a0bfc924c
laoguo <laoguo@qq.com>
2019-07-23 22:26:30 +0800

    商品添加以及框架搭建

--- FreshShop/Store/urls.py
+++ FreshShop/Store/urls.py
@@ -5,7 +5,10 @@ urlpatterns = [
     path('login/',login),
     path("index/",index),
     re_path("^$",index),
-    path("blank/",blank),
+    path("blank/",base),
     path("404/",error404),
-    path("qc/",loginOut)
+    path("qc/",loginOut),
+    path("register_store/",register_store),
+    path("add_goods/",add_goods),
+    path("goods_list/", goods_list)
 ]
--- FreshShop/Store/views.py
+++ FreshShop/Store/views.py
@@ -1,6 +1,7 @@
 import hashlib
 
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from django.shortcuts import HttpResponseRedirect
 
 from Store.models import *
@@ -37,6 +38,7 @@ def login(request):
                 if user.password==web_password and cookies=="login_page":
                     result=HttpResponseRedirect("/store/index/")
                     result.set_cookie("username",username)
+                    result.set_cookie("user_id",user.id)
                     request.session["username"]=username
                     return result
     return response
@@ -54,17 +56,119 @@ def loginValid(fun):
 
 @loginValid
 def index(request):
-    return render(request,"store/index.html")
+    user_id=request.COOKIES.get("user_id")
+    if user_id:
+        user_id=int(user_id)
+    else:
+        user_id=0
+    store=Store.objects.filter(user_id=user_id).first()
+    if store:
+        is_store = 1
+    else:
+        is_store = 0
+    return render(request,"store/index.html",{"is_store":is_store})
 
 def loginOut(request):
     response=HttpResponseRedirect("/store/login/")
     response.delete_cookie("username")
     return response
 
-def blank(request):
-    return render(request,"store/blank.html")
+def base(request):
+    return render(request, "store/base.html")
 
 def error404(request):
     return render(request,"store/404.html")
+
+
+def register_store(request):
+    type_list=StoreType.objects.all()
+    if request.method=="POST":
+        post_data=request.POST
+        store_name = post_data.get("store_name")
+        store_address = post_data.get("store_address")
+        store_description = post_data.get("store_description")
+        store_phone = post_data.get("store_phone")
+        store_money = post_data.get("store_money")
+
+        user_id=int(request.COOKIES.get("user_id"))
+        type_lists=post_data.getlist("type")
+
+        store_logo = request.FILES.get("store_logo")
+
+        store=Store()
+        store.store_name=store_name
+        store.store_address=store_address
+        store.store_description=store_description
+        store.store_phone=store_phone
+        store.store_money=store_money
+        store.user_id=user_id
+        store.store_logo=store_logo
+        store.save()
+
+        for i in type_lists:
+            store_type=StoreType.objects.get(id=i)
+            store.type.add(store_type)
+        store.save()
+    return render(request,"store/register_store.html",locals())
+
+def add_goods(request):
+    if request.method == "POST":
+        goods_name= request.POST.get("goods_name")
+        goods_price= request.POST.get("goods_price")
+        goods_number = request.POST.get("goods_number")
+        goods_description = request.POST.get("goods_description")
+        goods_date = request.POST.get("goods_date")
+        goods_safeDate = request.POST.get("goods_safeDate")
+        goods_store = request.POST.get("goods_store")
+        goods_image = request.FILES.get("goods_image")
+
+        goods=Goods()
+        goods.goods_name=goods_name
+        goods.goods_price = goods_price
+        goods.goods_number = goods_number
+        goods.goods_description = goods_description
+        goods.goods_date = goods_date
+        goods.goods_safeDate = goods_safeDate
+        goods.goods_image=goods_image
+        goods.save()
+        goods.store_id.add(
+            Store.objects.get(id=int(goods_store))
+        )
+        goods.save()
+    return render(request,"store/add_goods.html")
+
+
+def goods_list(request):
+    keywords=request.GET.get("keywords","")
+    page_num=request.GET.get("page_num",1)
+    if keywords:
+        goods_list=Goods.objects.filter(goods_name__contains=keywords)
+    else:
+        goods_list=Goods.objects.all()
+    paginator=Paginator(goods_list,3)
+    page=paginator.page(int(page_num))
+    page_range=paginator.page_range
+
+    count_goods=Goods.objects.all().count()
+
+    count=goods_list.count()
+    page_end=count//3 if count//3==0 else count//3+1
+
+    page_num=int(page_num)
+    if page_num ==page_end:
+        next_num=page_num
+    else:
+        next_num=page_num+1
+
+    if page_num == 1:
+        before_num=page_num
+    else:
+        before_num=page_num-1
+    return render(request,"store/goods_list.html",{"page":page,"page_range":page_range,"keywords":keywords,
+                                                   "page_num":page_num,"page_end":page_end,"count_goods":count_goods,
+                                                   "next_num":next_num,"before_num":before_num})
+
+
+
 
 # Create your views here.
